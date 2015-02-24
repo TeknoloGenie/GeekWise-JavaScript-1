04af25f07fc0d3643e36497cb0afe16dbe68278d
aaronrussell <aaron@teknologenie.com>
2015-02-23 16:35:19 -0800

    Day 3 uploaded and Course Outline added

--- JustDoIt/Day1/js/scripts.js
+++ JustDoIt/Day1/js/scripts.js
@@ -1,3 +1,10 @@
-function alertMsg() {
-    alert('Im inside of a function!');
+var julesHours = 365 * 4 * 4;
+var hallieHours = 365 * 6 * 2.5;
+
+if (julesHours > hallieHours) {
+    alert('Jules won!');
+} else if (julesHours == hallieHours) {
+    alert('Its a tie!');
+} else {
+    alert('Hallie won!');
 }
