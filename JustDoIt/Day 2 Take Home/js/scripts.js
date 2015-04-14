f4de021baa8b513435b71501e7c8058904ed6ab7
aaronrussell <aaron@teknologenie.com>
2015-04-14 14:39:50 -0700

    Day 5 uploaded

--- JustDoIt/Day 2 Take Home/js/scripts.js
+++ JustDoIt/Day 2 Take Home/js/scripts.js
@@ -10,7 +10,7 @@ function isMultipleFive(num){
     }
 }
 
-for(i=0;i<101;i++){
+for(var i=0;i<101;i++){
     if ( isMultipleThree(i) && isMultipleFive(i) ){
         console.log('FIZZBUZZ');
     } else if(isMultipleThree(i)){
